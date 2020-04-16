c336c16a715c78464dc7ce8e046f289b9d474f63
fumito-ikemoto <fumito-ikemoto@united.jp>
2020-04-16 14:20:48 +0900

    Initial commit

---
+++ app/src/main/java/jp/honorable/mention/weatherapp/MainActivity.kt
@@ -0,0 +1,87 @@
+package jp.honorable.mention.weatherapp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.os.AsyncTask
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+val URL: String = "http://weather.livedoor.com/forecast/webservice/json/v1"
+val CITYID:String = "city=130010"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            weatherTask().execute()
+        }
+    }
+
+    inner class weatherTask():AsyncTask<String, Void, String>()
+    {
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response:String?
+            try{
+                response = URL(URL + "?" + CITYID).readText(
+                    Charsets.UTF_8
+                )
+            }catch (e: Exception){
+                response = null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            try
+            {
+                val jsonObj = JSONObject(result)
+                val title = jsonObj.getString("title")
+
+                textView.text = title
+            }
+            catch(e:Exception)
+            {
+
+            }
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
